2524938817bc51b2398ba9badae97df6951cc6bb
funny-module-5140 <kevin.michael805@gmail.com>
2020-10-24 23:03:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -55,35 +55,17 @@ def load_library(path)
          :japanese =>  "(Ｔ▽Ｔ)"
     }
    }
-   
-
-
-def team_colors(team)
-  if team == "Charlotte Hornets" 
-    return game_hash[:away][:colors]
-  else return game_hash[:home][:colors]
-  end
 end
+
 
 def get_english_meaning(path, emoticon)
   load_library(path)
-  emoticon_hash = Hash.new
-
-  emoticon_hash["get_emoticon"] = Hash.new
-  emoticon_hash["get_meaning"] = Hash.new
-
-  emoticons.each do |english_word, emoticon_set|
-    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
-    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
-  end
-  emoticon_hash
-end
    path.each[emoticon] = 
-  end
+    
 end
 
 	
 def get_japanese_emoticon(path, emoticon)
   load_library(path)
- end
+  
 end
