8dffa18827b67efe2e7b093104410f204ddb997a
funny-module-5140 <kevin.michael805@gmail.com>
2020-10-24 22:42:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,70 @@
-# require modules here
-
-def load_library
-  # code goes here
+require 'yaml'
+def load_library(path)
+  library = YAML.load_file(path)
+{ 
+      'happy' => {
+         :english => ":)",
+         :japanese => "(＾ｖ＾)"
+      },
+       'angel' => {
+         :english => "O:)",
+         :japanese => "☜(⌒▽⌒)☞"
+      },
+      'angry' => {
+         :english => ">:(",
+         :japanese => "ヽ(ｏ`皿′ｏ)ﾉ "
+   },
+    'bored' => {
+         :english => ":O",
+         :japanese => "(ΘεΘ;)"
+   },
+    'embarrassed'=> {
+          :english => ":$",
+         :japanese => "(#^.^#)"
+   },
+    'confused'=> {
+          :english => "%)",
+         :japanese => "(゜.゜)"
+   },
+    'grinning'=> {
+          :english => "=D",
+         :japanese =>  "(￣ー￣)"
+   },
+    'fish'=> {
+          :english => "><>",
+         :japanese =>  ">゜))))彡"
+   },
+    'wink'=> {
+          :english => ";)",
+         :japanese =>  "(^_-)"
+   },
+    'surprised'=> {
+          :english => ":o",
+         :japanese =>  "o_O"
+   },
+    'glasses'=> {
+          :english => "8D",
+         :japanese =>  "(^0_0^)"
+   },
+     'kiss'=> {
+          :english => ":*",
+         :japanese =>  "(*^3^)/~☆"
+   },
+    'sad'=> {
+          :english => ":'(",
+         :japanese =>  "(Ｔ▽Ｔ)"
+    }
+   }
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+def get_english_meaning(path, emoticon)
+  library(path)
+    
 end
 
-def get_english_meaning
-  # code goes here
+	
+def get_japanese_emoticon(path, emoticon)
+  library(path)
+  
 end
