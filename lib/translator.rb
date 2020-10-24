c0611bd6ca29016e033f2bc1d3bf53d294af3e17
funny-module-5140 <kevin.michael805@gmail.com>
2020-10-24 22:45:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,64 +14,7 @@ def load_library(path)
   emoticons
 end
 
-require 'yaml'
-def load_library(path)
-  library = YAML.load_file(path)
-{ 
-      'happy' => {
-         :english => ":)",
-         :japanese => "(＾ｖ＾)"
-      },
-       'angel' => {
-         :english => "O:)",
-         :japanese => "☜(⌒▽⌒)☞"
-      },
-      'angry' => {
-         :english => ">:(",
-         :japanese => "ヽ(ｏ`皿′ｏ)ﾉ "
-   },
-    'bored' => {
-         :english => ":O",
-         :japanese => "(ΘεΘ;)"
-   },
-    'embarrassed'=> {
-          :english => ":$",
-         :japanese => "(#^.^#)"
-   },
-    'confused'=> {
-          :english => "%)",
-         :japanese => "(゜.゜)"
-   },
-    'grinning'=> {
-          :english => "=D",
-         :japanese =>  "(￣ー￣)"
-   },
-    'fish'=> {
-          :english => "><>",
-         :japanese =>  ">゜))))彡"
-   },
-    'wink'=> {
-          :english => ";)",
-         :japanese =>  "(^_-)"
-   },
-    'surprised'=> {
-          :english => ":o",
-         :japanese =>  "o_O"
-   },
-    'glasses'=> {
-          :english => "8D",
-         :japanese =>  "(^0_0^)"
-   },
-     'kiss'=> {
-          :english => ":*",
-         :japanese =>  "(*^3^)/~☆"
-   },
-    'sad'=> {
-          :english => ":'(",
-         :japanese =>  "(Ｔ▽Ｔ)"
-    }
-   }
-end
+
 
 def team_colors(team)
   if team == "Charlotte Hornets" 
